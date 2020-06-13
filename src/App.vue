aa894f3fc1ba58a4c943c9ef65ff22cb3aa5bb83
Maurice Oegerli <maurice@oeger.li>
2020-06-13 21:29:38 +0200

    version before canvas

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
     <input type="text" name id v-model="c1" />
     <input type="text" name id v-model="c2" />
     <button @click="calculate">calculate</button>
-    <p>Anzahl: {{list.length}}</p> 
+    <p v-if="doubleValues">Doppelte Werte: {{doubleValues}}</p>
+    <p>Anzahl: {{list.length}}</p>
+    <!-- <canvas ref="calcCanvas"></canvas> -->
     <p v-for="(entry, index) in list" :key="index">X: {{entry.zX}} Y: {{entry.zY}}</p>
   </div>
 </template>
@@ -21,25 +23,37 @@ export default {
       c2: 0,
       zX: 0,
       zY: 0,
-      list: []
+      list: [],
+      doubleValues: null
     };
   },
   methods: {
-    calculate() {
+    async calculate() {
       this.list = [];
       this.zX = 0;
       this.zY = 0;
       this.c1 = parseFloat(this.c1)
       this.c2 = parseFloat(this.c2)
-      for (
-        let index = 0;
-        index < 100 && this.zX <= 2 && this.zY <= 2;
-        index++
-      ) {
-        this.zX = this.zX * this.zX - this.zY * this.zY + this.c1;
-        this.zY = 2 * this.zX * this.zY + this.c2;
-        this.list.push({ zX: this.zX, zY: this.zY });
-      }
+
+      let duplicationArray = []
+
+      await setTimeout(() => {
+        for (
+          let index = 0;
+          index < 1000 && this.zX <= 2 && this.zY <= 2;
+          index++
+        ) {
+          this.zX = this.zX * this.zX - this.zY * this.zY + this.c1;
+          this.zY = 2 * this.zX * this.zY + this.c2;
+          this.list.push({ zX: this.zX, zY: this.zY });
+
+          if (duplicationArray.length > 0 && duplicationArray.find(e => e.zX === this.zX && e.zY === this.zY)) {
+            this.doubleValues = { zX: this.zX, zY: this.zY }
+            break;
+          }
+          duplicationArray.push({ zX: this.zX, zY: this.zY })
+        }
+      }, 100);
     }
   },
   computed: {
